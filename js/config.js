53b6d3d95f7bed9cb9866a4d14154f34732d1cf7
hxl240010 <hxl240010@utdallas.edu>
2025-04-15 15:26:57 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_cDGQHzLcd', 
-        userPoolClientId: '6dmru42o5kpo8bla3iegi7dqf7', 
-        region: 'us-east-1',
-        client_secret: 'k9nqjolgovt03vn7r77843ctfsjkalhrk5hpq3g8a2rmhk1alt9'
+        userPoolClientId: '6hler8vm63dsr3lo96s652a2a2', 
+        region: 'us-east-1'
+        
         
     },
     api: {
