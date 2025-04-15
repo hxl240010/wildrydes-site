2647b3b871f75cc94c3d56787b7c350d940bbc47
hxl240010 <hxl240010@utdallas.edu>
2025-04-15 15:03:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_cDGQHzLcd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6dmru42o5kpo8bla3iegi7dqf7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_cDGQHzLcd', 
+        userPoolClientId: '6dmru42o5kpo8bla3iegi7dqf7', 
+        region: 'us-east-1',
+        client_secret: 'k9nqjolgovt03vn7r77843ctfsjkalhrk5hpq3g8a2rmhk1alt9'
+        
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
